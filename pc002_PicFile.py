6582725224f0e6499f7ff758cdf90977d9891e88
rexfa <rex_fa@163.com>
2019-03-07 11:01:33 +0800

    HSV求平均

--- pc002_PicFile.py
+++ pc002_PicFile.py
@@ -11,6 +11,12 @@ class PicFileClass:
     #获取整个图片的HSV平均
     def HSVEven(self):
         imgHSV = cv2.cvtColor(self.__Img,cv2.COLOR_HSV2BGR)
+        #三维数组对第0轴求两次平均，就成功变成三个数的全局平均了。
         x0 = np.mean(imgHSV,axis=0)
         x00 = np.mean(x0,axis=0)
         return x00
+
+hclass = PicFileClass("zyl.jpg")
+
+outEven = hclass.HSVEven()
+print(outEven)
